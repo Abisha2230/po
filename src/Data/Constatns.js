67c696a52c300bef49e646b363e335ab3ec3d032
John Doe <johndoe@example.com>
2023-12-05 15:12:38 +0530

    ni

---
+++ src/Data/Constatns.js
@@ -0,0 +1,13 @@
+export const Bio = {
+    name:'Abinaya.R',
+    roles:[
+        "Software developer",
+        "Web developer",
+        "Full Stack Developer",
+        "App Developer",
+    ],
+    description: " I honed my skills in HTML CSS JavaScript, and React.js.My long-term goal is to become a full-stack developer, and I'm actively expanding myknowledge in back-end development, databases, andAPI integration.",
+   github:'https://github.com/Abisha2230',
+   Resume:'file:///C:/Users/abinaya/Downloads/Abinaya%20Front%20End%20developer.pdf',
+   Linkedin:''
+}
---
+++ src/Utils/Theme.js
@@ -0,0 +1,24 @@
+export const darkTheme = {
+    bg:'#1C1C27',
+    bgLight:'#1C1E27',
+    primary:'#854ce6',
+    text_primary:'#f2f3f4',
+    text_secondary:'#b1b2b3',
+    card:'#171721',
+    card_light:'#191924',
+   button:'#854ce6',  
+   white:'#ffff',
+   black:'#000'   
+
+}
+
+export const lightTheme = {
+    bg:'#ffffffff',
+    bgLight:'#f0f0f0',
+    primary:'#be1adb',
+    text_primary:'#11111111',
+    text_secondary:'#48494a',
+    card:'#fff',
+    button:'#5c5b5b'
+
+}
